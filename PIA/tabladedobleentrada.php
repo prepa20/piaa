ca80a022efd222eab6ff7b9f3dfd1230435e0af5
prepa20 <20preparatoria20@gmail.com>
2025-05-20 11:10:00 -0600

    Delete PIA directory

--- PIA/tabladedobleentrada.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <head>
-        <title>TABLA DE DOBLE ENTRADA</title>
-    </head>
-    <link rel="stylesheet" href="styles_tabla_de_doble_entrada.css">
-    <body>
-        <button class="regresar"onclick="location.href='e3.php'">Regresar</button>
-    </body>
-</html>
